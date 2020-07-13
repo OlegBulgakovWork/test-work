7f87a1a12a8346bd48e54ed031b1e47e325034eb
Oleg Bulgakov <obulgakovwork@gmail.com>
2020-07-13 17:04:35 +0300

    Updates

---
+++ precache-manifest.b16c6090f5735b0afba9c88c6681fa09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d829dfada905f8ebf9188e4d87850253",
+    "url": "/test-work/index.html"
+  },
+  {
+    "revision": "31f56dcf00eb64704792",
+    "url": "/test-work/static/css/main.f2f69681.chunk.css"
+  },
+  {
+    "revision": "2aa4ddb3f02455901f26",
+    "url": "/test-work/static/js/2.0928ce5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-work/static/js/2.0928ce5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f56dcf00eb64704792",
+    "url": "/test-work/static/js/main.f5b6ccd8.chunk.js"
+  },
+  {
+    "revision": "5cc6100e0f4225a16f13",
+    "url": "/test-work/static/js/runtime-main.8761b134.js"
+  }
+]);
